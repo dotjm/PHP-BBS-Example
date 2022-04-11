eea048cbc664d54df9b109ec9a42fd8ebdac07a6
정지민(Jeong Jimin) <46292908+jimin7777@users.noreply.github.com>
2022-04-11 15:45:10 +0900

    Add memo

--- ex1/view.php
+++ ex1/view.php
@@ -8,6 +8,12 @@
     // echo "<pre>";
     // print_r($rs);
 
+    $query="select * from memo where status=1 and bid=".$rs->bid." order by memoid asc";
+    $memo_result = $mysqli->query($query) or die("query error => ".$mysqli->error);
+    while($mrs = $memo_result->fetch_object()){
+        $memoArray[]=$mrs;
+    }
+
 ?>
 
 <!doctype html>
@@ -47,5 +53,72 @@
 
     </div>
 
-</body>
+    <div style="margin-top:20px;">
+      <form class="row g-3">
+        <div class="col-md-10">
+          <textarea class="form-control" placeholder="댓글을 입력해주세요." id="memo" style="height: 60px"></textarea>
+        </div>
+        <div class="col-md-2">
+          <button type="button" class="btn btn-secondary" id="memo_button">댓글등록</button>
+        </div>
+      </form>
+    </div>
+    <div id="memo_place">
+      <!-- <div class="card mb-4" style="max-width: 100%;margin-top:20px;">
+        <div class="row g-0">
+          <div class="col-md-2  text-dark bg-light">
+           
+          </div>
+          <div class="col-md-10">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div> -->
+      <?php
+        foreach($memoArray as $ma){
+      ?>
+        <div class="card mb-4" style="max-width: 100%;margin-top:20px;">
+          <div class="row g-0">
+            <div class="col-md-12">
+              <div class="card-body">
+                <p class="card-text"><?php echo $ma->memo;?></p>
+                <p class="card-text"><small class="text-muted"><?php echo $ma->userid;?> / <?php echo $ma->regdate;?></small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      <?php }?>
+    </div>
+
+    <script>
+      $("#memo_button").click(function () {
+      
+            var data = {
+                memo : $('#memo').val() ,
+                bid : <?php echo $bid;?>
+            };
+                $.ajax({
+                    async : false ,
+                    type : 'post' ,
+                    url : 'memo_write.php' ,
+                    data  : data ,
+                    dataType : 'html' ,
+                    error : function() {} ,
+                    success : function(return_data) {
+                      if(return_data=="member"){
+                        alert('로그인 하십시오.');
+                        return;
+                      }else{
+                        $("#memo_place").append(return_data);
+                      }
+                    }
+            });
+        });
+    </script>
+
+  </body>
 </html>    
